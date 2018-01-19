d48befc6f83abc9980a4e146f192a9358bf70c9b
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-01-19 03:08:32 -0700

    retrieve-users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,18 @@
 require "httparty"
+require 'json'
 
 class Kele
   include HTTParty
 
   def initialize(email, password)
-    self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
+    response = self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
     raise "Invalid email or password" if response.code == 404
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
   end
 
 private
