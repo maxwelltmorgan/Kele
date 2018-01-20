d3e6214ddfbda70c1b4ef8de8ad365f351057b87
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-01-20 01:11:52 -0700

    messaging

--- lib/kele.rb
+++ lib/kele.rb
@@ -22,6 +22,19 @@ class Kele
     JSON.parse(response.body)
   end
 
+  def get_messages(page = nil)
+    if page == nil
+      response = self.class.get(base_api_endpoint("message_threads"), headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get(base_api_endpoint("message_threads?page=#{page}"), headers: { "authorization" => @auth_token })
+    end
+    JSON.parse(response.body)
+  end
+
+  def create_message(email, recipient_id, subject, message)
+    response = self.class.post(base_api_endpoint("messages"), body: { "sender": email, "recipient_id": recipient_id, "subject": subject, "stripped-text": message }, headers: { "authorization" => @auth_token })
+  end
+
 private
 
   def base_api_endpoint(end_point)
