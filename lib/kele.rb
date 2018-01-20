e596c8f9dd26555d5dfeeec5d4726a34fa36c9e7
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-01-20 00:17:41 -0700

    roadmaps checkpoints

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,10 @@
 require "httparty"
 require 'json'
+require './lib/roadmap'
 
 class Kele
   include HTTParty
+  include Roadmap
 
   def initialize(email, password)
     response = self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
