8c5ba6d518029161da3356a31dd1f1a0a96c47dc
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-01-19 23:41:29 -0700

    mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -15,6 +15,11 @@ class Kele
     JSON.parse(response.body)
   end
 
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
 private
 
   def base_api_endpoint(end_point)
