f5045e211741168fa849f2cf3136b5175e338cf1
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-01-22 23:49:17 -0700

    checkpoint submission complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -35,8 +35,8 @@ class Kele
     response = self.class.post(base_api_endpoint("messages"), body: { "sender": email, "recipient_id": recipient_id, "subject": subject, "stripped-text": message }, headers: { "authorization" => @auth_token })
   end
 
-  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
-    response = self.class.post(base_api_endpoint("checkpoint_submissions"), body: { "checkpoint_id": checkpoint_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment, "enrollment_id": enrollment_id }, headers: { "authorization" => @auth_token })
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post(base_api_endpoint("checkpoint_submissions"), body: { "checkpoint_id": checkpoint_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment, "enrollment_id": self.get_me["current_enrollment"]["id"] }, headers: { "authorization" => @auth_token })
   end
 
 private
